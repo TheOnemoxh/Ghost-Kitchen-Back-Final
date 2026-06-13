0de469ebda25e4833f38a39cd8ba29383adefcb0
Adrianco205 <adrianangul267@gmail.com>
2026-06-13 11:18:32 -0500

    perf(db): optimizar pool de conexiones para alta concurrencia
    - SQLAlchemy: Se aumentó pool_size a 50, max_overflow a 50 y se configuraron timeouts y reciclaje (pre_ping) para mayor estabilidad.
    - Docker Compose: Se elevó max_connections a 600 y shared_buffers a 256MB en PostgreSQL para soportar la prueba de estrés.

--- app/core/database.py
+++ app/core/database.py
@@ -3,15 +3,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Toma la URL del docker-compose. Si no existe, usa una por defecto.
 SQLALCHEMY_DATABASE_URL = os.getenv(
     "DATABASE_URL",
     "postgresql://ghost_user:ghost_password@localhost/ghost_kitchen_db"
 )
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Configuración agresiva para alto tráfico
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_size=50,        # Conexiones base
+    max_overflow=50,     # Conexiones de emergencia (Total: 100 simultáneas)
+    pool_timeout=120,    # Dale hasta 2 minutos a la fila para responder bajo estrés extremo
+    pool_recycle=1800,   # Reinicia conexiones cada 30 min para evitar desconexiones fantasma de red
+    pool_pre_ping=True   # Verifica que la conexión esté viva antes de usarla (Evita fallos en picos)
+)
 
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
